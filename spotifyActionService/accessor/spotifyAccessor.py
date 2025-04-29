151cfdd4474b08a086cea2e311d34b3f71995241
Josh Prier (JPrier) <JoshKPrier@gmail.com>
2025-04-28 19:16:13 -0700

    Implement sync functionality

--- spotifyActionService/accessor/spotifyAccessor.py
+++ spotifyActionService/accessor/spotifyAccessor.py
@@ -1,20 +1,8 @@
+from typing import List, Dict, Any
 from spotifyActionService.util.logger import logger
 from spotifyActionService.accessor.spotifyClient import spotify_client as client
 
-def convert_playlist_to_ordered_id_list(playlist_items: list) -> list:
-    """
-    Convert playlist items to a list of track IDs, sorted by the timestamp when they were added (ascending).
-    Each item requires 'added_at' in ISO format and a nested 'track.id'.
-    """
-    # Filter out any non-track items and sort by added_at timestamp
-    sorted_items = sorted(
-        (item for item in playlist_items if item.get("track") and item.get("added_at")),
-        key=lambda i: i["added_at"]
-    )
-    return [item["track"]["id"] for item in sorted_items]
-
-
-def fetch_playlist_tracks(playlist_id: str) -> list:
+def fetch_playlist_tracks(playlist_id: str) -> List[Dict[str, Any]]:
     """
     Fetch all items from a Spotify playlist, including each item's 'added_at' timestamp.
     """
@@ -22,10 +10,63 @@ def fetch_playlist_tracks(playlist_id: str) -> list:
     # Request only the fields we need to minimize payload
     resp = client.playlist_items(
         playlist_id,
-        fields="items(added_at,track(id)),next"
+        fields="items(added_at,track(id)),next",
     )
+    logger.info(f"Fetched playlist items page 0: {resp}")
     tracks.extend(resp.get("items", []))
     while resp.get("next"):
         resp = client.next(resp)
+        logger.info(f"Fetched playlist items page: {resp}")
         tracks.extend(resp.get("items", []))
+    logger.info(f"Fetched {len(tracks)} tracks from playlist {playlist_id}: {tracks}")
     return tracks
+
+def add_tracks_to_playlist(playlist_id: str, track_ids: List[str]) -> None:
+    """
+    Add tracks to a Spotify playlist.
+    """
+    logger.info(f"Adding tracks to playlist {playlist_id}: {track_ids}")
+    try:
+        response = client.playlist_add_items(playlist_id, track_ids)
+        logger.info(f"Added tracks to playlist {playlist_id}: {response}")
+    except Exception as e:
+        logger.error(f"Failed to add tracks to playlist {playlist_id}: {e}")
+        raise
+
+# ----- DOES NOT WORK ------
+# Spotify has changed the API and the positions field is ignored.
+# Currently, there is no info on whether this will be fixed or not.
+# For now this will be left here for reference
+
+# def remove_tracks_from_playlist(playlist_id: str, snapshot_id: str, duplicates: Dict[str, List[int]]) -> None:
+#     """
+#     Remove duplicate tracks from a Spotify playlist.
+#     duplicates = {track_id: [position1, position2, ...]}
+#     """
+#     payload = [
+#         { "uri": f"spotify:track:{track_id}", "positions": positions }
+#         for track_id, positions in duplicates.items()
+#     ]
+#     logger.info(f"Removing duplicates from playlist {playlist_id}-{snapshot_id}: {payload}")
+#     try:
+#         response = client.playlist_remove_specific_occurrences_of_items(
+#             playlist_id,
+#             payload,
+#             snapshot_id=snapshot_id
+#         )
+#         logger.info(f"Removed duplicates from playlist {playlist_id}-{snapshot_id}: {response}")
+#     except Exception as e:
+#         logger.error(f"Failed to remove duplicates {playlist_id}-{snapshot_id}:{payload} received error: {e}")
+#         raise
+
+def get_metadata(playlist_id: str) -> Dict[str, Any]:
+    """
+    Fetch metadata of a Spotify playlist.
+    """
+    try:
+        metadata = client.playlist(playlist_id, fields="id,name,description,snapshot_id")
+        logger.info(f"Fetched metadata for playlist {playlist_id}: {metadata}")
+        return metadata
+    except Exception as e:
+        logger.error(f"Failed to fetch metadata for playlist {playlist_id}: {e}")
+        raise
--- spotifyActionService/logic/playlistRefreshLogic.py
+++ spotifyActionService/logic/playlistRefreshLogic.py
@@ -1,21 +1,65 @@
 from spotifyActionService.util.logger import logger
-from spotifyActionService.accessor.spotifyAccessor import fetch_playlist_tracks, convert_playlist_to_ordered_id_list
+from spotifyActionService.accessor.spotifyAccessor import fetch_playlist_tracks, add_tracks_to_playlist, get_metadata
+from spotifyActionService.logic.mapper.spotifyMapper import map_to_id_set
 from spotifyActionService.models.actions import ActionType, Action, SyncAction, ArchiveAction
+
+# def collect_duplicates(playlist_items: list) -> dict[str, list[int]]:
+#     """
+#     Collect duplicates from the playlist items.
+#     Returns a dictionary where the keys are track IDs and the values are lists of positions of duplicates.
+#     """
+#     # Create a dictionary to store the occurrences of each track ID
+#     # Occurences -> {track_id: [{position: ..., added_at: ...}, ...]}
+#     occurrences: dict[str, list[dict]] = {}
+#     for index, item in enumerate(playlist_items):
+#         track_id = item["track"]["id"]
+#         occurrences.setdefault(track_id, []).append({
+#                 "position": index,
+#                 "added_at": item["added_at"]
+#             })
+        
+#     # Collect dictionary of duplicates
+#     # duplicates = {track_id: [position1, position2, ...]}
+#     duplicates: dict[str, list[int]] = {}
+#     for track_id, items in occurrences.items():
+#         # If there are no duplicates, skip
+#         if len(items) <= 1:
+#             continue
+#         # Sort by 'added_at', remove the oldest from the list and keep the rest
+#         to_remove = sorted(items, key=lambda o: o["added_at"])[1:]
+#         duplicates[track_id] = [item["position"] for item in to_remove]
+
+#     return duplicates
+
 
 def sync_playlists(action: SyncAction) -> None:
     """
     Synchronize the source playlist with the target playlist by copying items from the source to the target.    
     """
-    logger.info("Fetching target playlist items...")
-    target_items = fetch_playlist_tracks(action.target_playlist_id)
-    target_ids = convert_playlist_to_ordered_id_list(target_items)
-
     logger.info("Fetching source playlist items...")
     source_items = fetch_playlist_tracks(action.source_playlist_id)
-    source_ids = convert_playlist_to_ordered_id_list(source_items)
+    source_ids = map_to_id_set(source_items)
 
-    logger.info(f"Found {len(source_ids)} tracks in source playlist (oldest→newest): {source_ids}")
-    logger.info(f"Found {len(target_ids)} tracks in target playlist (oldest→newest): {target_ids}")
+    logger.info("Fetching target playlist items...")
+    target_items = fetch_playlist_tracks(action.target_playlist_id)
+    target_ids = map_to_id_set(target_items)
+
+    # if len(target_ids) < len(target_items) and action.avoid_duplicates:
+    #     logger.warning("Target playlist has duplicates removing duplicates...")
+    #     duplicates = collect_duplicates(target_items)
+    #     if duplicates:
+    #         remove_tracks_from_playlist(action.target_playlist_id, snapshot_id, duplicates)
+            
+    logger.info(f"Found {len(source_ids)} tracks in source playlist: {source_ids}")
+    logger.info(f"Found {len(target_ids)} tracks in target playlist: {target_ids}")
+
+    # Find which source tracks aren’t yet in the target
+    tracks_to_add = [track_id for track_id in source_ids if track_id not in target_ids]
+    logger.info(f"Tracks to add: {tracks_to_add}")
+
+    add_tracks_to_playlist(action.target_playlist_id, tracks_to_add)
+    logger.info(f"Added {len(tracks_to_add)} tracks to target playlist: {action.target_playlist_id}")
+
 
 def archive_playlists(action: ArchiveAction) -> None:
     """
@@ -23,11 +67,8 @@ def archive_playlists(action: ArchiveAction) -> None:
     """
     logger.info("Fetching source playlist items...")
     source_items = fetch_playlist_tracks(action.source_playlist_id)
-    source_ids = convert_playlist_to_ordered_id_list(source_items)
+    source_ids = map_to_id_set(source_items)
 
-    logger.info(f"Found {len(source_ids)} tracks in source playlist (oldest→newest): {source_ids}")
+    logger.info(f"Found {len(source_ids)} tracks in source playlist: {source_ids}")
 
     logger.info("Archiving source playlist...")
-
-if __name__ == "__main__":
-    sync_playlists()

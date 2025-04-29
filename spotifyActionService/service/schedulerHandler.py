8fb5931f411922db8132a35a05a64a9e8c03b2a1
Josh Prier (JPrier) <JoshKPrier@gmail.com>
2025-04-28 17:14:16 -0700

    Fix schedulerHandler

--- spotifyActionService/service/schedulerHandler.py
+++ spotifyActionService/service/schedulerHandler.py
@@ -1,15 +1,19 @@
 import time
 import schedule
-from logic.playlistRefreshLogic import sync_playlists
-from util.logger import logger
-from util.env import get_env
+from spotifyActionService.service.helper.actionHelper import parseActionFile, handleActions
+from spotifyActionService.util.logger import logger
+from spotifyActionService.util.env import get_env
 
 # Setup Constants
 INTERVAL = int(get_env("SYNC_INTERVAL_MINUTES", "60"))
 SLEEP_TIME_IN_SECONDS = 5
 
 def main():
-    sync_playlists()
+    logger.info(f"Parsing action file...")
+    actions = parseActionFile("spotifyActionService/actions.json")
+    logger.info(f"Parsed {len(actions)} actions.")
+    logger.info(f"Actions: {actions}")
+    handleActions(actions)
 
 
 if __name__ == "__main__":
